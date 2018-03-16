1c95c1bd19c39248d0f08c528c7f2cc189c26cb7
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2018-03-16 16:34:48 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -1,29 +1,3 @@
-#include <algorithm>
-#include <cassert>
-
-class vector_t
-{
-private:
-	int * elements_;
-	std::size_t size_;
-	std::size_t capacity_;
-public:
-	vector_t();
-	vector_t(vector_t const & other);
-	vector_t & operator =(vector_t const & other);
-	~vector_t();
-
-	std::size_t size() const;
-	std::size_t capacity() const;
-
-	void push_back(int value);
-	void pop_back();
-
-	int & operator [](std::size_t index);
-	int operator [](std::size_t index) const;
-
-	bool operator ==(vector_t const & other) const;
-};
 
 vector_t::vector_t()
 {
