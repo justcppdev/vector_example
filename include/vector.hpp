aeaeb941adf9f1e1e14579593ccf4adf1d59c56d
TheDonten <32191011+TheDonten@users.noreply.github.com>
2018-03-22 19:55:15 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-class vector_t
+template <typename T> class vector_t
 {
 private:
-	int * elements_;
+	T * elements_;
 	std::size_t size_;
 	std::size_t capacity_;
 public:
@@ -23,16 +23,17 @@ public:
 
 	bool operator ==(vector_t const & other) const;
 };
+template <typename T>
+bool operator !=(vector_t<T> const & lhs, vector_t<T> const & rhs);
 
-bool operator !=(vector_t const & lhs, vector_t const & rhs);
-
-vector_t::vector_t() {
+template <typename T> 
+vector_t<T>::vector_t() {
   size_ = 0;
   capacity_ = 0;
   elements_ = nullptr;
 }
-
-vector_t::vector_t(vector_t const& other) {
+template <typename T>
+vector_t<T>::vector_t(vector_t const& other) {
   this->size_ = other.size_;
   this->capacity_ = other.capacity_;
   this->elements_ = new int[capacity_];
@@ -41,7 +42,7 @@ vector_t::vector_t(vector_t const& other) {
   }
 }
 
-vector_t& vector_t::operator=(vector_t const& other) {
+vector_t<T>& vector_t<T>::operator=(vector_t const& other) {
   if (this != &other) {
     delete[] elements_;
     this->size_ = other.size_;
@@ -53,8 +54,8 @@ vector_t& vector_t::operator=(vector_t const& other) {
   }
   return *this;
 }
-
-bool vector_t::operator==(vector_t const& other) const {
+template <typename T>
+bool vector_t<T>::operator==(vector_t const& other) const {
   if (this->size_ == other.size_) {
     for (std::size_t i = 0; i < size_; i++) {
       if (this->elements_[i] != other.elements_[i]) {
@@ -68,18 +69,20 @@ bool vector_t::operator==(vector_t const& other) const {
   else
     return false;
 }
-
-vector_t::~vector_t() {
+template <typename T>
+vector_t<T>::~vector_t() {
   delete[] elements_;
   size_ = 0;
   capacity_ = 0;
 }
+template <typename T>
+std::size_t vector_t<T>::size() const { return size_; }
 
-std::size_t vector_t::size() const { return size_; }
+template <typename T>
+std::size_t vector_t<T>::capacity() const { return capacity_; }
 
-std::size_t vector_t::capacity() const { return capacity_; }
-
-void vector_t::push_back(int value) {
+template <typename T>
+void vector_t<T>::push_back(int value) {
   if (size_ == 0) {
     size_ = 1;
     capacity_ = 1;
@@ -100,8 +103,8 @@ void vector_t::push_back(int value) {
     size_++;
   }
 }
-
-void vector_t::pop_back() {
+template <typename T>
+void vector_t<T>::pop_back() {
   size_--;
   if (size_ == 0 || size_ * 4 == capacity_) {
     int* mas;
@@ -119,11 +122,14 @@ void vector_t::pop_back() {
   }
 }
 
-int& vector_t::operator[](std::size_t index) { return elements_[index]; }
+template <typename T>
+int& vector_t<T>::operator[](std::size_t index) { return elements_[index]; }
 
-int vector_t::operator[](std::size_t index) const { return elements_[index]; }
+template <typename T>
+int vector_t<T>::operator[](std::size_t index) const { return elements_[index]; }
 
-bool operator!=(vector_t const& lhs, vector_t const& rhs) {
+template <typename T>
+bool operator!=(vector_t<T> const& lhs, vector_t<T> const& rhs) {
   if (lhs == rhs) {
     return false;
   }
