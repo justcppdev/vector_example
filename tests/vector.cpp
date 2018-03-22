61229b595aebebb6063e69a97e02a0c918c8c476
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2018-03-23 00:05:35 +0300

    Update vector.cpp

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -126,3 +126,129 @@ TEST_CASE("poping elements")
 	vector.push_back(9); 
 	REQUIRE_THROWS_AS(vector.at(4), std::out_of_range); 
 }
+
+//float
+
+TEST_CASE("creating vector")
+{
+	vector_t<float> vector;
+	REQUIRE( vector.size() == 0 );
+	REQUIRE( vector.capacity() == 0 );
+}
+
+TEST_CASE("copying vector")
+{
+	vector_t<float> vector;
+	vector.push_back(1.2);
+
+	vector_t<float> copy(vector);
+	REQUIRE( copy == vector );
+}
+
+TEST_CASE("assigning vector")
+{
+	vector_t<float> vector1;
+	vector_t<float> vector2;
+
+	vector1.push_back(1.1);
+	vector2.push_back(2.2);
+
+	vector1 = vector2;
+	REQUIRE( vector1 == vector2 );
+}
+
+TEST_CASE("equaling vector")
+{
+	vector_t<float> vector1;
+	vector_t<float> vector2;
+
+	vector1.push_back(1.3);
+	vector2.push_back(1.3);
+
+	REQUIRE( vector1 == vector2 );
+
+	vector1.push_back(2.3);
+	REQUIRE( vector1 != vector2 );
+}
+
+TEST_CASE("indexing vector")
+{
+	vector_t<float> vector;
+
+	vector.push_back(1.4);
+
+	REQUIRE( vector[0] == 1.4 );
+
+	vector_t<float> const copy(vector);
+	REQUIRE( copy[0] == 1.4 );
+}
+
+TEST_CASE("pushing elements")
+{
+	vector_t<float> vector;
+
+	vector.push_back(1.1);
+	REQUIRE( vector.size() == 1 );
+	REQUIRE( vector.capacity() == 1 );
+
+	vector.push_back(2.2);
+	REQUIRE( vector.size() == 2 );
+	REQUIRE( vector.capacity() == 2 );
+
+	vector.push_back(3.3);
+	REQUIRE( vector.size() == 3 );
+	REQUIRE( vector.capacity() == 4 );
+
+	vector.push_back(4.1);
+	REQUIRE( vector.size() == 4 );
+	REQUIRE( vector.capacity() == 4 );
+
+	vector.push_back(5.2);
+	REQUIRE( vector.size() == 5 );
+	REQUIRE( vector.capacity() == 8 );
+}
+
+TEST_CASE("poping elements")
+{
+	vector_t<float> vector;
+
+	vector.push_back(1.0);
+	vector.push_back(2.0);
+	vector.push_back(3.3);
+	vector.push_back(4.2);
+	vector.push_back(5.1);
+	vector.push_back(6.4);
+
+	vector.pop_back();
+	REQUIRE( vector.size() == 5 );
+	REQUIRE( vector.capacity() == 8 );
+
+	vector.pop_back();
+	REQUIRE( vector.size() == 4 );
+	REQUIRE( vector.capacity() == 8 );
+
+	vector.pop_back();
+	REQUIRE( vector.size() == 3 );
+	REQUIRE( vector.capacity() == 8 );
+
+	vector.pop_back();
+	REQUIRE( vector.size() == 2 );
+	REQUIRE( vector.capacity() == 4 );
+
+	vector.pop_back();
+	REQUIRE( vector.size() == 1 );
+	REQUIRE( vector.capacity() == 2 );
+
+	vector.pop_back();
+	REQUIRE( vector.size() == 0 );
+	REQUIRE( vector.capacity() == 1 );
+}
+
+	TEST_CASE("error") 
+{ 
+	vector_t<float> vector; 
+	vector.push_back(1.4);
+	vector.push_back(5.2); 
+	vector.push_back(8.7); 
+	REQUIRE_THROWS_AS(vector.at(4), std::out_of_range); 
+}
