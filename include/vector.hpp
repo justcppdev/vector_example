d774a1358c55f4edca90ab41254446b2083c12c7
TheDonten <32191011+TheDonten@users.noreply.github.com>
2018-03-29 22:24:33 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -73,7 +73,7 @@ class tree_t {
 
     print(stream, level + 1, node->left);
   }
-  void act(std::ostream& stream, char op, int value) {
+  void operation(std::ostream& stream, char op, int value) {
     switch (op) {
       case '+': {
         insert(value);
