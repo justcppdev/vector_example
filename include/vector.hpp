ad0c66eb2bede5e7903d04bbd27a6c5f1276fca0
TheDonten <32191011+TheDonten@users.noreply.github.com>
2018-03-30 01:39:22 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -106,23 +106,11 @@ class tree_t {
     }
   }
   void destroy(node_t* node) {
-    if (root_!= nullptr) {
-      node_t* time = node;
-      while (time != nullptr) {
-        if (time->left != nullptr) {
-          time = time->left;
-        } else if (time->right != nullptr) {
-          time = time->right;
-        } else if (time == node && node->left == nullptr &&
-                   node->right == nullptr) {
-          delete time;
-          break;
-        } else if (time->left == nullptr && time->right == nullptr) {
-          delete time;
-          time = node;
-        }
-      }
+    if(node != nullptr){
+      destroy(node->left);
+     destroy(node->right);
     }
+   delete node;
   }
   ~tree_t() { destroy(root_); }
 };
