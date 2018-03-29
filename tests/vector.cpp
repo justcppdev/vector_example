3706d3af6343ba6cb693251cbc5b3ceab5a8801c
TheDonten <32191011+TheDonten@users.noreply.github.com>
2018-03-29 22:27:37 +0300

    Update vector.cpp

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -8,3 +8,51 @@ TEST_CASE("creating tree")
 	tree_t tree1;
 	REQUIRE( tree1.root() == nullptr );
 }
+
+TEST_CASE("insert")
+{
+  tree_t tree1;
+    tree1.insert(5);
+    tree1.insert(4);
+    tree1.insert(6); 
+    tree1.insert(7);
+	std::string out {
+	        "------7\n"
+		"---6\n"
+		"5\n"
+		"---4\n"
+	};
+	
+	std::ostringstream stream;
+	tree.print (stream, tree.set_pointer());
+	
+	REQUIRE(stream.str() == out);
+	
+}
+
+TEST_CASE("find")
+{
+  tree_t tree1;
+  tree1.insert(5);
+  tree1.insert(4);
+  tree1.insert(6); 
+  tree1.insert(7);
+  REQUIRE(tree.find(5) == true);
+  REQUIRE(tree.find(8) == false);
+	
+}
+TEST_CASE("operations")
+{
+ tree.operation('+', 5);
+ tree.operation('+', 4);
+ tree.operation('+', 6);	
+	std::string out {
+		"--------6\n"
+		"----5\n"
+		"--------4\n"
+	};
+	std::ostringstream stream;
+	tree.operation ('=', stream);
+	
+	REQUIRE(stream.str() == out);
+}
