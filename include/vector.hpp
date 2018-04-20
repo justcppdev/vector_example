4a8ea2aca26d2efdcbf301c3cd4c73ab7b8e6041
TheDonten <32191011+TheDonten@users.noreply.github.com>
2018-04-20 11:38:39 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,190 +1,208 @@
 #include <iostream>
+using namespace std;
 template <typename T>
 class tree_t {
- private:
-  struct node_t {
-    node_t* left;
-    node_t* right;
-    T value;
-  };
+private:
+    struct node_t {
+        node_t* left;
+        node_t* right;
+        T value;
+    };
 
- private:
-  node_t* root_;
+private:
+    node_t* root_;
 
- public:
-  tree_t() { root_ = nullptr; }
+public:
+    tree_t() { root_ = nullptr; }
 
-  node_t* root() const { return root_; }
- bool isEmpty()
-	{
-		if(root_==nullptr)
-		{
-			return true;
-		}
-	return false;
-	}
- tree_t(std::initializer_list<T> keys)
-{
-	int n = keys.size();
-	const int* _ptr = keys.begin();
-	for (int i=0; i < n; i++)
-	{
-		insert(_ptr[i]);
-	}
-}
-bool equally(node_t* first, node_t* second ) const{
-         if( first == nullptr && second == nullptr) return true;
-     
-         else if( first != nullptr && second != nullptr && first->value == second->value){
-             return equally(first->left,second->left) && equally(first->right, second->right);
-         }
-        
-         else return false;
-     }
-     auto operator==(tree_t const & other) const{
-         return(equally(root_,other.root()));
-     }
-	
-  void insert(T value) {
-    node_t* node = new node_t;
-    node->value = value;
-    node->right = nullptr;
-    node->left = nullptr;
-    if (root_ == nullptr) {
-      root_ = node;
-      return;
-    }
-
-    node_t* vetka = root_;
-    while (vetka != nullptr) {
-      if (vetka->value < value) {
-        if (vetka->right != nullptr) {
-          vetka = vetka->right;
-        } else {
-          vetka->right = node;
-          return;
+    node_t* root() const { return root_; }
+    tree_t(std::initializer_list<int> keys) : tree_t() {
+        for (auto key : keys) {
+            insert(key);
         }
-      } else if (vetka->value > value) {
-        if (vetka->left != nullptr) {
-          vetka = vetka->left;
-        } else {
-          vetka->left = node;
-          return;
+    }
+    void insert(T value) {
+        node_t* node = new node_t;
+        node->value = value;
+        node->right = nullptr;
+        node->left = nullptr;
+        if (root_ == nullptr) {
+            root_ = node;
+            return;
         }
-      } else
-        return;
+
+        node_t* vetka = root_;
+        while (vetka != nullptr) {
+            if (vetka->value < value) {
+                if (vetka->right != nullptr) {
+                    vetka = vetka->right;
+                } else {
+                    vetka->right = node;
+                    return;
+                }
+            } else if (vetka->value > value) {
+                if (vetka->left != nullptr) {
+                    vetka = vetka->left;
+                } else {
+                    vetka->left = node;
+                    return;
+                }
+            } else
+                return;
+        }
     }
-  }
-  bool find(T value) const {
-    node_t* node = root_;
-    while (node != nullptr) {
-      if (node->value == value) {
-        return true;
-      } else {
-        if (value <= node->value) {
-          node = node->left;
-        } else
-          node = node->right;
-      }
-    }
-    return false;
-  }
-
-  void print(std::ostream& stream, int level, node_t* node) {
-    if (node == nullptr) return;
-
-    print(stream, level + 1, node->right);
-
-    for (unsigned int i = 0; i < level; i++) {
-      stream << "---";
-    }
-    stream << node->value << std::endl;
-
-    print(stream, level + 1, node->left);
-  }
- bool remove(T value) {
-        if (!find(value))
-            return false;
+    bool find(T value) const {
         node_t* node = root_;
-        node_t* q = nullptr; // родитель элемента, которого нам нужно удалить
-        while( (node!=nullptr) && (node->value!=value)) {
-            q = node;
-            if(value < node->value) node=node->left;
-            else node=node->right;
-
-        }
-        node_t* v; // установить в сортировочном виде дерева.
-        
-        if (node->left==nullptr)  v = node->right;
-        else if ( node->right==nullptr)  v = node->left;
-        
-        else  {
-            node_t* s;
-            node_t* t;
-            t= node;
-            v=node->right;
-            s=v->left;
-            while(s != nullptr){
-                t=v;
-                v= s;
-                s =  v->left;
+        while (node != nullptr) {
+            if (node->value == value) {
+                return true;
+            } else {
+                if (value <= node->value) {
+                    node = node->left;
+                } else
+                    node = node->right;
             }
-           if(t !=node){
-               t->left = v->right;
-               v->left = node->left;
-               v->right = node->right;
-           }
-           else {
-               v->left = node->left;
-               
-           }
-    }
-        if(q == nullptr){
-            root_ = v;
         }
-        else if ( node == q->right) q->right = v;
-        else q->left = v;
-        delete node;
-        return true;
- }
-	
-  void act(char op, T value, std::ostream& stream) {
-    switch (op) {
-      case '?': {
-        if (find(value)) {
-          stream << "true";
+        return false;
+    }
+
+    void print(std::ostream& stream, int level, node_t* node) const {
+        if (node == nullptr) return;
+
+        print(stream, level + 1, node->right);
+
+        for (unsigned int i = 0; i < level; i++) {
+            stream << "---";
+        }
+        stream << node->value << endl;
+
+        print(stream, level + 1, node->left);
+    }
+    void operation(std::ostream& stream, char op, int value) {
+        switch (op) {
+            case '+': {
+                insert(value);
+                break;
+            }
+            case '?': {
+                find(value);
+                break;
+            }
+            case '=': {
+                print(stream, 0, root_);
+                break;
+            }
+            case 'q': {
+                exit(0);
+                break;
+            }
+            default: { cout << "invalid operation"; }
+        }
+    }
+    bool remove(T value) {
+        auto pair = find_(value);
+        if (!pair.first) {
+            return false;
+        }
+
+        auto removed_node = pair.first;
+        auto removed_node_parent = pair.second;
+
+        if (!removed_node->left && !removed_node->right) {
+            if(removed_node == removed_node_parent->right) removed_node_parent->right = nullptr;
+            if(removed_node == removed_node_parent->left) removed_node_parent->left = nullptr;
+        } else if (removed_node->left && !removed_node->right) {
+            transplant(removed_node->left, removed_node_parent, false);
+        } else if (removed_node->right && !removed_node->left) {
+            transplant(removed_node->right, removed_node_parent, false);
+        }
+
+        else {  //когда есть два сына у удаляемого элемента
+            auto pair = min_(removed_node->right, removed_node);
+            auto leftest_node = pair.first;
+            auto leftest_node_parent = pair.second;
+
+            transplant(leftest_node->right, leftest_node_parent, false);
+            if(leftest_node == leftest_node_parent->right) leftest_node_parent->right = nullptr;
+            if(leftest_node == leftest_node_parent->left) leftest_node_parent->left = nullptr;
+            transplant(leftest_node, removed_node_parent, true);
+
+        }
+
+        delete removed_node;
+
+    }
+
+    std::pair<node_t*, node_t*> find_(T value) {
+        node_t* node = root_;
+        node_t* parent = nullptr;  // родитель элемента, которого нам нужно удалить
+        while ((node != nullptr) && (node->value != value)) {
+            parent = node;
+            if (value < node->value)
+                node = node->left;
+            else
+                node = node->right;
+        }
+        return {node, parent};
+    };
+
+    std::pair<node_t*, node_t*> min_(node_t* node, node_t* parent) {
+        assert(node);
+
+        while (node->left) {
+            parent = node;
+            node = node->left;
+        }
+        return {node, parent};
+    };
+    void transplant(node_t* node, node_t* parent, bool move_children) {
+        if (!node) {
+            return;
+        }
+        node_t* child = nullptr;
+        if (parent == nullptr) {
+            node->left = root_->left;
+            node->right = root_->right;
+            root_ = node;
+
+        } else {
+            if (parent->value < node->value) {
+                child = parent->right;
+                parent->right = node;
+            } else {
+                child = parent->left;
+                parent->left = node;
+            }
+
+            if (move_children && child) {
+                 node->left = child->left;
+                 node->right = child->right;
+                child->left = nullptr;
+                child->right = nullptr;
+            }
+        }
+    }
+    void destroy(node_t* node) {
+        if (node != nullptr) {
+            destroy(node->left);
+            destroy(node->right);
+            delete node;
+        }
+    }
+    ~tree_t() { destroy(root_); }
+    bool euqal(node_t* lhs, node_t* rhs) const {
+        if (lhs == nullptr && rhs == nullptr)
+            return true;
+
+        else if (lhs != nullptr && rhs != nullptr && lhs->value == rhs->value) {
+            return (euqal(lhs->left, rhs->left) && euqal(lhs->right, rhs->right));
         } else
-          stream << "false";
-        break;
-      }
-      case '=': {
-        print(stream, 0, root_);
-        break;
-      }
+            return false;
+    }
 
-      default: { std::cout << "invalid operation"; }
+    bool operator==(tree_t const& other) const {
+        return euqal(root_, other.root_);
     }
-  }
 
-  void act1(char op, T value) {
-    switch (op) {
-      case '+': {
-        insert(value);
-        break;
-      }
-      case 'q': {
-        exit(0);
-        break;
-      }
-      default: { std::cout << "invalid operation"; }
-    }
-  }
-  void destroy(node_t* node) {
-    if (node != nullptr) {
-      destroy(node->left);
-      destroy(node->right);
-      delete node;
-    }
-  }
-  ~tree_t() { destroy(root_); }
+    void print(std::ostream& stream) const { print(stream, 0, root_); }
 };
